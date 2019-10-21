8f34240cda27ec58bb86750e64559a8f30ad1b17
Nielson Rolim <nielson.rolim@gmail.com>
2019-10-21 15:54:38 -0300

    Added an Agent to cache word_list

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,4 +1,4 @@
 defmodule Dictionary do
-  defdelegate start(), to: Dictionary.Wordlist
+  defdelegate start(), to: Dictionary.Wordlist, as: :start_link
   defdelegate random_word(words), to: Dictionary.Wordlist
 end
--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,14 +1,17 @@
 defmodule Dictionary.Wordlist do
+  def start_link() do
+    { :ok, agent } = Agent.start_link(&word_list/0)
+    agent
+  end
 
-  def start do
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
+  end
+
+  defp word_list do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
     |> File.read!()
     |> String.split(~r/\n/)
-  end
-
-  def random_word(words) do
-    words
-    |> Enum.random()
   end
 end
